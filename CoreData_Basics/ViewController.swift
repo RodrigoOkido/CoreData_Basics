9fa3856160d50a384c7f5a250e8caafd85def409
RodrigoOkido <rodrigookido@gmail.com>
2022-03-16 17:58:41 -0300

    Initial Commit

---
+++ CoreData_Basics/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreData_Basics
+//
+//  Created by Rodrigo Yukio Okido on 16/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
