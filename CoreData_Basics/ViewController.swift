92276b5c1268f9a5a4484aecf8bdf6d30c67b3ca
RodrigoOkido <rodrigookido@gmail.com>
2022-03-17 15:22:56 -0300

    Base structure implemented. Core Data entities created.

--- CoreData_Basics/ViewController.swift
+++ CoreData_Basics/ViewController.swift
@@ -6,14 +6,46 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
 
+    
+    var persistentContainer: NSPersistentContainer {
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            fatalError()
+        }
+        
+        return appDelegate.persistentContainer
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    
+    /**
+     Create some Person data to include in our CoreData.
+     */
+    func createPersonData() {
+        
+    }
+    
+    
+    /**
+     Create some Car data to include in our CoreData.
+     */
+    func createCarData() {
+        
+    }
+    
+    
+    /**
+     Create some Movie data to include in our CoreData.
+     */
+    func createMovieData() {
+        
+    }
 }
 
